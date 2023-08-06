0a769c3f5c1b32e069249cdaf8f65966c0f472a4
ruru-dev <rurutia89@gmail.com>
2023-08-06 01:51:33 -0500

    add like functionality

--- src/App.js
+++ src/App.js
@@ -1,54 +1,53 @@
 // Importing dependencies.
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 // Importing styles.
-import './App.css';
+import "./App.css";
 
 // Importing components.
-import BeerCard from './BeerCard';
+import BeerCard from "./BeerCard";
 
 class App extends Component {
- constructor(){
-  super()
-  this.state = {
-    arrayOfBeer : []
-  };
- };
+  constructor() {
+    super();
+    this.state = {
+      arrayOfBeer: [],
+    };
+  }
 
-// The componentDidMount() method runs after the component output has been rendered to the DOM.
-componentDidMount(){
-  axios.get('https://api.punkapi.com/v2/beers')
-    .then(res => {
-      const arrayOfBeer = res.data
-      this.setState({ arrayOfBeer })
-    })
-};
+  // The componentDidMount() method runs after the component output has been rendered to the DOM.
+  componentDidMount() {
+    axios.get("https://api.punkapi.com/v2/beers").then((res) => {
+      const arrayOfBeer = res.data;
+      this.setState({ arrayOfBeer });
+    });
+  }
 
-render(){
-return (
-  <div className="App">
-    <header className="App-header">
-      <ol> 
-        { this.state.arrayOfBeer.map((beer,index) => {
-        return (
-          <BeerCard 
-            key={index} 
-            name={beer.name} 
-            image_url={beer.image_url} 
-            first_brewed={beer.first_brewed}
-            tagline={beer.tagline}
-            abv={beer.abv}
-            description={beer.description}
-          />
-        )
-      })
-      };
-      </ol>
-    </header>
-  </div> 
-);
-};
-};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ol>
+            {this.state.arrayOfBeer.map((beer, index) => {
+              return (
+                <BeerCard
+                  key={index}
+                  name={beer.name}
+                  image_url={beer.image_url}
+                  first_brewed={beer.first_brewed}
+                  tagline={beer.tagline}
+                  abv={beer.abv}
+                  description={beer.description}
+                />
+              );
+            })}
+            ;
+          </ol>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/BeerCard.js
+++ src/BeerCard.js
@@ -1,20 +1,32 @@
 // In order to create a new component in React we need to import React.
-import React from 'react';
+import { useState } from 'react';
+
+// Importing css specific to this component.
+import './BeerCard.css';
 
 // Importing our icon componenet.
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as faHeartRegular} from '@fortawesome/free-regular-svg-icons';
-// import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 
 // Our function declaration.
 function BeerCard(props) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLiked = () => {
+    setIsLiked(current => !current);
+  }
+  
   return (
-    <li style={ {listStyle: "none"} }>
-      <img style={ {height: "200px"} } src={props.image_url}></img>
+    <li className="beer-card">
+      <img src={props.image_url}></img>
       <h3> {props.name} <span> {props.first_brewed} </span></h3>
       <h4> {props.tagline} </h4>
       <p> <span> {props.abv} </span> {props.description} </p>
-      <FontAwesomeIcon icon={faHeartRegular} style={{color: "#9680b7",}} size="2xl" />
+      <FontAwesomeIcon icon={isLiked ? faHeartSolid : faHeartRegular} 
+                       style={{color: "#9680b7",}} 
+                       size="2xl" 
+                       onClick={toggleLiked}/>
     </li>
   )
 };
